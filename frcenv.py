b32915bc44b8dfd694aeed3e17b46edaa1ee46ef
TreeBronch <74438588+TreeBronch@users.noreply.github.com>
2024-01-12 21:53:47 -0500

    Added reward for achieving goal. Added experimental wandb callback

--- frcenv.py
+++ frcenv.py
@@ -27,7 +27,7 @@ class FRCEnv(gym.Env):
 
         self.state = None
         self.reward_weights = [
-            0.25, 0.25, 0, 0,0,0,0.25,0,0,0, 50, 0
+            0.25, 0.25, 0, 0,0,0,0.25,0,0,0, 50, -50
         ]+self.internal_env.lidar_zeros
 
         self.top_reward = -math.inf
